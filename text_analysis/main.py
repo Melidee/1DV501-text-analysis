6ec88e4c61bd76925da878867368a03cbc5543e6
Amelia Rossi <w.ardecan@gmail.com>
2025-10-21 17:04:54 +0200

    little menu

--- text_analysis/main.py
+++ text_analysis/main.py
@@ -1,35 +1,47 @@
+import os
 from io import TextIOWrapper
 from string import whitespace, ascii_letters
 from typing import Self
 
 from text_analysis.stats import Statistics
 
+
 def main():
     print("Hello from text_analysis")
-    book_path = "./alice-in-wonderland.txt"
+    book_path = pick_book()
     with Book(book_path) as book:
         stats = Statistics()
         for word in book:
             stats.add(word)
-        print(f"There are {stats.mo()} unique words in {book_path}")
+        print(f"There are {stats.most_common_words()} unique words in {book_path}")
+
+
+def pick_book() -> str:
+    files = os.listdir()
+    print("\n".join(f"{i}: {name}" for (i, name) in enumerate(files)))
+    chosen = int(input("Enter number for the file: "))
+    return files[chosen]
+
 
 class Book:
     def __init__(self, book_path: str) -> None:
         self.book: TextIOWrapper = open(book_path, "r")
         self.tail: str = ""
         self.words: list[str] = []
-        
+
     def read_chunk(self) -> None:
-        words = self.tail + self.book.read(1024)
-        if words and words[-1] not in whitespace:
-            [words, *tail] = words.rsplit(maxsplit=1)
-            self.tail = tail[0] if tail else ""
-        words = ''.join(ch.casefold() for ch in words if ch in ascii_letters + whitespace)
-        self.words = words.split()
-        
+        words = self.book.read(1024)
+        if not words:
+            return
+        while (ch := self.book.read(1)) and ch != whitespace:
+            words += ch
+        self.words = "".join(
+            ch for ch in words if ch in ascii_letters + whitespace
+        ).split()
+
     def __iter__(self) -> Self:
         return self
-        
+
     def __next__(self) -> str:
         if not self.words:
             self.read_chunk()
@@ -37,12 +49,13 @@ class Book:
             return self.words.pop(0)
         else:
             raise StopIteration
-    
+
     def __enter__(self) -> Self:
         return self
-        
+
     def __exit__(self, type: type, value, traceback) -> None:
-        self.book.close()        
+        self.book.close()
+
 
 if __name__ == "__main__":
     main()
--- text_analysis/stats.py
+++ text_analysis/stats.py
@@ -1,14 +1,14 @@
 class Statistics:
     def __init__(self) -> None:
         self.words: dict[str, int] = dict()
-        
+
     def add(self, word: str) -> None:
         current_word_count = count if (count := self.words.get(word)) else 0
         self.words[word] = current_word_count + 1
-        
+
     def unique_word_count(self) -> int:
         return len(self.words.keys())
-        
-    def most_common_words(self, number:int=10) -> list[tuple[str, int]]:
+
+    def most_common_words(self, number: int = 10) -> list[tuple[str, int]]:
         ordered = sorted(self.words.items(), key=lambda e: e[1])
         return ordered[::-1][0:number]
